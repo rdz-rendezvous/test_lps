a706788773dd1a9540f52a0fb92cd9b49759f02b
rdz-rendezvous <147297616+rdz-rendezvous@users.noreply.github.com>
2023-10-08 12:45:59 +0700

    Add files via upload

---
+++ TestLPS/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Globalization;
+using System.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Soal B");
+
+        int inputInt;
+        Console.Write("Input: ");
+        inputInt = Convert.ToInt32(Console.ReadLine());
+
+        string inputSoalB = inputInt.ToString();
+
+        int length = inputSoalB.Length;
+
+        foreach(var input in inputSoalB)
+        {
+            string angka = "";
+            for(int i = 1; i < length; i++)
+            {
+                angka += "0";
+            }
+            Console.WriteLine(input + angka);
+            length--;
+        }
+
+
+        Console.WriteLine("Soal C");
+        Console.Write("Input: ");
+
+        string inputSoalC;
+        inputSoalC = Console.ReadLine();
+        string result = string.Empty;
+
+        for(int i = 0; i<inputSoalC.Length; i++)
+        {
+            if (!result.Contains(inputSoalC[i]))
+            {
+                result += inputSoalC[i];
+            }
+        }
+
+        result = result.Replace(" ", "");
+
+        foreach (var input in result)
+        {
+            int ctr = 0;
+            for (int i = 0; i < inputSoalC.Length; i++)
+            {
+                if (input == inputSoalC[i])
+                {
+                    ctr++;
+                }
+
+            }
+            Console.WriteLine(input + "-" + ctr);
+        }
+
+        Console.WriteLine("Soal D");
+        Console.Write("Input: ");
+
+        int inputSoalD;
+        inputSoalD = Convert.ToInt32(Console.ReadLine());
+
+
+        for(int i = 1; i<= inputSoalD; i++)
+        {
+            if (i % 5 == 0 && i != 5)
+            {
+                Console.Write("IDIC ");
+            }
+            else if (i % 6 == 0 && i != 6)
+            {
+                Console.Write("LPS ");
+            }
+            else
+            {
+                Console.Write(i+" ");
+            }
+        }
+
+        Console.WriteLine("Soal E");
+        Console.Write("Input: ");
+
+        string inputSoalE;
+           
+        inputSoalE = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(inputSoalE))
+        {
+            Console.WriteLine("Format Judul: " + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(inputSoalE.ToLower()));
+
+            Console.WriteLine("Format Biasa : " + inputSoalE[0] + inputSoalE.Substring(1).ToLower());
+        }
+
+
+
+    }
+}
